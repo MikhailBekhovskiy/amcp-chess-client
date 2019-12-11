8ed2e78e2f4b9c356292cc2f55938f5e80dedf3d
MukkuMayc <st063775@student.spbu.ru>
2019-12-11 11:25:38 +0300

    Set up aws amplify

---
+++ src/config.js
@@ -0,0 +1,12 @@
+export default {
+    apiGateway: {
+      REGION: "eu-central-1",
+      URL: "https://jh60lfzxch.execute-api.eu-central-1.amazonaws.com/dev"
+    },
+    cognito: {
+      REGION: "eu-central-1",
+      USER_POOL_ID: "eu-central-1_VSClWwKUW",
+      APP_CLIENT_ID: "4i3kcqp160nhb6ni9hor2qb8pr",
+      IDENTITY_POOL_ID: "eu-central-1:09835806-d8e0-4e5d-8168-7080d0d4951a"
+    }
+  };
