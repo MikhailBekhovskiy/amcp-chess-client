74206b152cd42df41f32de5337b803c61dcde9cc
MukkuMayc <st063775@student.spbu.ru>
2019-12-13 11:44:21 +0300

    Add web socket functions

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -1,21 +1,21 @@
 import React, {useState} from "react";
 import { Prompt } from "react-router";
 import { API } from "aws-amplify";
-import { Button } from "react-bootstrap";
-import "./Game.css"
+import "./Game.css";
+import config from "../config";
+import { FormGroup, FormControl } from "react-bootstrap";
+import LoaderButton from "../components/LoaderButton";
 
 export default function Game(props) {
+  const [isLoading, setIsLoading] = useState(false);
   props.userHasJoinedGame(true);
 
-  async function leaveGame() {
-    console.log('gameId:', `${props.match.params.id}`);
-    props.userHasJoinedGame(false);
-    await API.del("chess", `/games/${props.match.params.id}`);
-    return true;
-  }
-
+  const [text, setText] = useState("");
   let [messages, setMessages] = useState([])
 
+  function validateForm() {
+    return text.length > 0;
+  }
 
   function MessagesContainer(props) {
     return (
@@ -26,9 +26,43 @@ export default function Game(props) {
     )
   }
 
-  function handleClick() {
-    setMessages(messages.concat('test message'))
+  let socket;
+
+  function connectToWebSocket() {      
+    let webSocketConnection = config.apiGateway.URL;
+    console.log(webSocketConnection);
+    socket = new WebSocket(webSocketConnection);
+    socket.onmessage = function(event) {
+      setMessages(messages.concat(event.data));
+    };
+    socket.onerror = function(event) {
+        console.error("WebSocket error observed:", event);
+    };
   }
+
+  function sendMessage() {
+    let payload = { "action": "onMessage", "message": text };
+    socket.send(JSON.stringify(payload));
+  }
+
+  function disconnect() {
+    socket.close();
+  }
+
+  async function leaveGame() {
+    console.log("gameId:", `${props.match.params.id}`);
+    props.userHasJoinedGame(false);
+    await API.del("chess", `/games/${props.match.params.id}`);
+    return true;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    sendMessage();
+    // setMessages(messages.concat(text));
+  }
+
+  connectToWebSocket();
 
   return (
     <>
@@ -38,8 +72,26 @@ export default function Game(props) {
           message={leaveGame}
         />
       </div>
+      <form onSubmit={handleSubmit}>
+        <FormGroup controlId="content">
+          <FormControl
+            value={text}
+            componentClass="textarea"
+            onChange={e => setText(e.target.value)}
+          />
+        </FormGroup>
+        <LoaderButton
+          block
+          type="submit"
+          bsSize="large"
+          bsStyle="primary"
+          isLoading={isLoading}
+          disabled={!validateForm()}
+        >
+          Send
+        </LoaderButton>
+      </form>
       <MessagesContainer messages={messages}/>
-      <Button onClick={handleClick}/>
     </>
   )
 }
