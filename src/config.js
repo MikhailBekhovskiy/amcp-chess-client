68e4394f66c11d7410b690b18ffe6a74c88be1db
MukkuMayc <st063775@student.spbu.ru>
2019-12-14 14:10:05 +0300

    Add ws to config

--- src/config.js
+++ src/config.js
@@ -1,12 +1,13 @@
 export default {
     apiGateway: {
       REGION: "eu-central-1",
-      URL: "https://13n7jkxw26.execute-api.eu-central-1.amazonaws.com/dev"
+      URL: "https://13n7jkxw26.execute-api.eu-central-1.amazonaws.com/dev",
+      WS: "wss://pphgn476lb.execute-api.eu-central-1.amazonaws.com/dev",
     },
     cognito: {
       REGION: "eu-central-1",
       USER_POOL_ID: "eu-central-1_hCbYvuEUm",
       APP_CLIENT_ID: "6tb6p1khtmmq6vvgg279s4dh5m",
-      IDENTITY_POOL_ID: "eu-central-1:90f3464c-2907-42f7-ad28-685e99e81373"
+      IDENTITY_POOL_ID: "eu-central-1:90f3464c-2907-42f7-ad28-685e99e81373",
     }
   };
