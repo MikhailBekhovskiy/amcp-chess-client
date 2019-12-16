54a7d79935d68c55f5bf6fe5adf298a4648a6154
MukkuMayc <st063775@student.spbu.ru>
2019-12-17 02:57:58 +0300

    Update config

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
 export default {
     apiGateway: {
       REGION: "eu-central-1",
-      URL: "https://13n7jkxw26.execute-api.eu-central-1.amazonaws.com/dev",
-      WS: "wss://pphgn476lb.execute-api.eu-central-1.amazonaws.com/dev",
+      URL: "https://qh07nhidve.execute-api.eu-central-1.amazonaws.com/dev",
+      WS: "wss://07lk2rj3i7.execute-api.eu-central-1.amazonaws.com/dev",
     },
     cognito: {
       REGION: "eu-central-1",
-      USER_POOL_ID: "eu-central-1_hCbYvuEUm",
-      APP_CLIENT_ID: "6tb6p1khtmmq6vvgg279s4dh5m",
-      IDENTITY_POOL_ID: "eu-central-1:90f3464c-2907-42f7-ad28-685e99e81373",
+      USER_POOL_ID: "eu-central-1_sCMpoCFo2",
+      APP_CLIENT_ID: "3khsh883ngocv1c1kpklgmrk1f",
+      IDENTITY_POOL_ID: "eu-central-1:da8ff3f9-8814-4dee-8128-6c26de954437",
     }
   };
