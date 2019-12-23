df60f92921b93634a303a436487a190ee741026b
MikhailBekhovskiy <mbexovskij@gmail.com>
2019-12-23 12:22:25 +0300

    Updates

---
+++ precache-manifest.ac97dc5c126b2a5982def81ac5908947.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c92ec0da3afd613ee2d23aff548388fd",
+    "url": "/amcp-chess-client/index.html"
+  },
+  {
+    "revision": "a227712dfe5dd9f6b5af",
+    "url": "/amcp-chess-client/static/css/main.866fbf5e.chunk.css"
+  },
+  {
+    "revision": "c9aef631d3e169cd79b7",
+    "url": "/amcp-chess-client/static/js/2.e462091c.chunk.js"
+  },
+  {
+    "revision": "ca88389def8ad01cc13973aa5bf94c8a",
+    "url": "/amcp-chess-client/static/js/2.e462091c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a227712dfe5dd9f6b5af",
+    "url": "/amcp-chess-client/static/js/main.5f640603.chunk.js"
+  },
+  {
+    "revision": "790e21ab959d64f3844e",
+    "url": "/amcp-chess-client/static/js/runtime-main.abc8fe8f.js"
+  }
+]);
